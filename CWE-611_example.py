0da6448780f2185e92f49908c6ddb80f8b911227
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 15:19:33 +0800

    Update CWE-611_example.py

--- CWE-611_example.py
+++ CWE-611_example.py
@@ -1,10 +1,10 @@
-# xxe_parser.py
-from xml.dom.minidom import parse
+# xxe.py
+import xml.dom.minidom
 
 def parse_user_xml():
-    path = input("XML filepath: ")
-    # CWE-611: DOM parser預設啟用外部實體 → python/xml-external-entity
-    doc = parse(path)
+    fname = input("XML filename: ")
+    # CWE-611: 預設啟用外部實體解析
+    doc = xml.dom.minidom.parse(fname)
     print("Root element:", doc.documentElement.tagName)
 
 if __name__ == "__main__":
