034444ca56ea3c1e7738221c993f53a7dddeeaac
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 21:21:38 +0800

    Update CWE-79_example.py

--- CWE-79_example.py
+++ CWE-79_example.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     msg = request.args.get('msg', '')
-    return f"<h1>Your message: {msg}</h1>"  # CWE-79: 任意 HTML 輸出
+    return f"<h1>Your message: {msg}</h1>"  
 
 if __name__ == "__main__":
     app.run(port=5000)
