667653e7387272f50dbd009afe664057e8e50fc0
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 15:20:15 +0800

    Update CWE-326_example.py

--- CWE-326_example.py
+++ CWE-326_example.py
@@ -1,11 +1,11 @@
 # weak_crypto.py
 import hashlib
 
-def sign():
+def sign_data():
     data = input("Data to sign: ")
-    # CWE-326: 使用 MD5 作為訊息摘要，已知不安全
-    sig = hashlib.md5(data.encode()).hexdigest()
-    print("Signature (MD5):", sig)
+    # CWE-326: 使用 MD5 作為雜湊演算法
+    h = hashlib.md5(data.encode()).hexdigest()
+    print("MD5:", h)
 
 if __name__ == "__main__":
-    sign()
+    sign_data()
