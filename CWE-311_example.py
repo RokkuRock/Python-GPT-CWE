6f1984fa17ecc6431ea33c47bf8cfe76a7841f4f
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 21:20:50 +0800

    Update CWE-311_example.py

--- CWE-311_example.py
+++ CWE-311_example.py
@@ -3,7 +3,6 @@ import requests
 
 def fetch():
     url = input("Enter HTTPS URL: ")
-    # CWE-311: verify=False 會跳過 SSL 憑證檢查
     r = requests.get(url, verify=False)
     print("Status code:", r.status_code)
 
