78749597f430860c6d159993fa135b617894e710
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 21:20:36 +0800

    Update CWE-22_example.py

--- CWE-22_example.py
+++ CWE-22_example.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 @app.route("/download")
 def download():
     fname = request.args.get("file", "")
-    # CWE-22: 未過濾 '../'，直接使用者控制路徑
     path = os.path.join("uploads", fname)
     if not os.path.isfile(path):
         abort(404)
