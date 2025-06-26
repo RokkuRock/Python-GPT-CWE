2744599ed0640b981022e4b011766ecfa080efac
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 14:25:23 +0800

    Update CWE-22_example.py

--- CWE-22_example.py
+++ CWE-22_example.py
@@ -1,9 +1,9 @@
 # file: path_traversal.py
 def read_file():
-    fn = input("Filename in safe/ to read: ")
-    path = f"safe/{fn}"
-    with open(path, 'r') as f:
-        print("Content:", f.read())
+    fn = input("Filename under data/: ")
+    # CWE-22: 未過濾 ../
+    with open(f"data/{fn}", 'r') as f:
+        print(f.read())
 
 if __name__ == "__main__":
     read_file()
