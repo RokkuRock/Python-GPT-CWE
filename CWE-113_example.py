0e886c893b452fb5bed1d4c0ec44957b5467357b
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 21:20:21 +0800

    Update CWE-113_example.py

--- CWE-113_example.py
+++ CWE-113_example.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 @app.route('/setcookie')
 def setcookie():
     user = request.args.get('user', '')
-    # CWE-113: 直接將 user input 寫入 header，可能注入 CRLF
     resp = Response("Cookie set")
     resp.headers['Set-Cookie'] = f"user={user}"
     return resp
