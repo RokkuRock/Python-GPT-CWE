19f6f56620a5dc2ebfc23c8b091aacb2a49b232a
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 14:42:57 +0800

    Update CWE‑78_example.py

--- CWE‑78_example.py
+++ CWE‑78_example.py
@@ -1,10 +1,10 @@
-# file: cmd_injection.py
+# cmd_injection.py
 import subprocess
 
-def list_dir():
-    path = input("Directory to list: ")
-    # CWE-78: shell=True + 未清洗輸入
-    subprocess.call(f"ls {path}", shell=True)
+def list_files():
+    directory = input("Directory to list: ")
+    # CWE-78: shell=True 且未清洗使用者輸入，可注入命令
+    subprocess.run(f"ls {directory}", shell=True)
 
 if __name__ == "__main__":
-    list_dir()
+    list_files()
