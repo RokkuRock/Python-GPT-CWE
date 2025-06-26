e4a5f781df14cf3573884dd926e3b4ea097e749a
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 14:43:40 +0800

    Update CWE-94_example.py

--- CWE-94_example.py
+++ CWE-94_example.py
@@ -1,9 +1,9 @@
-# file: code_eval.py
-def calc():
+# code_eval.py
+def calculator():
     expr = input("Enter expression: ")
-    # CWE-94: 任意 eval
+    # CWE-94: 任意 eval，可執行惡意程式碼
     result = eval(expr)
     print("Result:", result)
 
 if __name__ == "__main__":
-    calc()
+    calculator()
